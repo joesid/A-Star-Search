f0065d248d8a798162e52467646c1f706b4bac3f
joesid <joesidious@gmail.com>
2023-08-25 23:59:40 +0100

    Add project files.

---
+++ A-Star-Search.cpp
@@ -0,0 +1,14 @@
+﻿// A-Star-Search.cpp : Defines the entry point for the application.
+
+#include "A-Star-Search.h"
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	vector<vector<int>> boards(5);
+
+
+	return 0;
+}
