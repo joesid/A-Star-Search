20dcff9be80f139c8a4d8c815c755881387920bd
joesid <joesidious@gmail.com>
2023-09-09 20:15:24 +0100

    Create ParseLine Function & Modify ReadBoardFile

--- A-Star-Search.cpp
+++ A-Star-Search.cpp
@@ -2,7 +2,7 @@
 
 #include "A-Star-Search.h"
 #include <vector>
-#include <iostream>
+#include <sstream>
 using namespace std;
 
 int main()
@@ -14,8 +14,70 @@ int main()
 		                           {0, 1, 0, 0, 0, 0},
 		                           {0, 1, 0, 0, 0, 0} };
 
-
-	 printBoard(board);
+     printBoard(board);
+    cout << " " << endl;
+    ReadBoardFile("1.board");
 
 	return 0;
+}
+
+void printBoard(vector<vector<int>>& grid)
+{
+    // Prints Vector of Vector ints in 2d Board Format
+    for (size_t i = 0; i < grid.size(); ++i)
+    {
+        cout << " " << endl;
+        for (size_t j = 0; j < grid[i].size(); ++j)
+        {
+            cout << grid[i][j] << " ";
+        }
+    }
+
+    cout << endl;
+}
+
+void ReadBoardFile(const string& filePath) {
+    //Open the file for reading
+    ifstream inputFile(filePath);
+
+    if (!inputFile.is_open()) {
+        cerr << "Failed to open the file. checking directory location might help " << endl;
+    }
+
+    string line;
+
+    while (getline(inputFile, line)) {
+
+        vector<int> parsedData = ParseLine(line);
+        //istringstream iss(line);
+
+        //string content = iss.str();
+        //cout << content << endl;
+
+        for(int num: parsedData) {
+            cout << num << " " ;
+        }
+        cout << "\n" ;
+    }
+
+
+    inputFile.close();
+}
+
+vector<int> ParseLine(const string& line) {
+    vector<int> result;
+    istringstream iss(line);
+    char comma;
+
+    int num;
+    while (iss >> num >> comma) {
+        result.push_back(num);
+
+        if (iss >> num) {
+            result.push_back(num);
+        }
+    }
+
+    return result;
+
 }
--- A-Star-Search.h
+++ A-Star-Search.h
@@ -7,40 +7,10 @@
 #include <vector>
 #include<fstream>
 #include<string>
+#include<sstream>
 
 using namespace std;
 
 void printBoard(vector<vector<int>>& grid);
 void ReadBoardFile(const string& filePath);
-
-void printBoard(vector<vector<int>>& grid)
-{
-	// Prints Vector of Vector ints in 2d Board Format
-	for (size_t i = 0; i < grid.size(); ++i)
-	{
-		cout << " " << endl;
-		for (size_t j = 0; j < grid[i].size(); ++j)
-		{
-			cout << grid[i][j] << " ";
-		}
-	}
-
-	cout << endl;
-}
-
-void ReadBoardFile(const string& filePath) {
-    //Open the file for reading
-    ifstream inputFile(filePath);
-
-    if (!inputFile.is_open()) {
-        cerr << "Failed to open the file. " << endl;
-    }
-
-    string line;
-
-    while (getline(inputFile, line)) {
-        cout << line << endl;
-    }
-
-    inputFile.close();
-}
+vector<int> ParseLine(const string& line);
