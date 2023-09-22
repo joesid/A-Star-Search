0b369ceafba8b8f41afc73da72f7fc54a4061eb8
joesid <joesidious@gmail.com>
2023-09-22 20:00:30 +0100

    Define State enum  - Create CellString Function

--- A-Star-Search.h
+++ A-Star-Search.h
@@ -14,3 +14,21 @@ using namespace std;
 void printBoard(vector<vector<int>>& grid);
 void ReadBoardFile(const string& filePath);
 vector<int> ParseLine(const string& line);
+
+
+
+ enum State {
+     kEmpty,
+     kObstacle
+ };
+
+ string CellString(State val)
+ {
+     if (val == kObstacle )
+     {
+         return "⛰️";
+     }
+     else {
+         return "0 ";
+     }
+ }
